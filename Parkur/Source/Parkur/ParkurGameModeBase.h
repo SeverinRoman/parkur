6a0c2c038c2b1d2e5760f4189462b3e11a1ac713
SeverinRoman <roman.s@ninjapromo.io>
2023-04-08 16:31:12 +0300

    - init

---
+++ Parkur/Source/Parkur/ParkurGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParkurGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKUR_API AParkurGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
